8472f2644f5ec8ee4915d3f2f1b2c4bb47e8a21f
rahulb03 <122718029+rahulb03@users.noreply.github.com>
2024-08-04 15:05:47 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -13,19 +13,19 @@ module.exports = (phase) => {
 
     const env = {
         API_PROD_URL: (() => {
-            if (isDev) return 'http://77.37.47.144:4200/api/frontend/'
+            if (isDev) return 'https://api.gymis.in/api/frontend/'
             // if (isDev) return 'http://localhost:3000/api/'
             if (isProd) {
                 // Note: The code below needs to be uncommented, and you should use your domain where your API is hosted.
                 // return 'Enter Your URL here'
-                return 'http://77.37.47.144:4200/api/frontend/'
+                return 'https://api.gymis.in/api/frontend/'
                 // return 'http://localhost:3000/api/'
             }
-            if (isStaging) return 'http://77.37.47.144:4200/api/frontend/'
+            if (isStaging) return 'https://api.gymis.in/api/frontend/'
             // if (isStaging) return 'http://localhost:3000/api/'
             return 'RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)'
         })(),
-        API_BASE_URL: 'http://77.37.47.144:4200/api/frontend/',
+        API_BASE_URL: 'https://api.gymis.in/api/frontend/',
     }
     const redirects = () => {
         return [
